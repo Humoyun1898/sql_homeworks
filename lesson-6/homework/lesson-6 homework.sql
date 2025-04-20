31d6bf159e1139758475eb4d83968aff32f5d027
Humoyun1898 <humoyunserwes@gmail.com>
2025-04-20 12:09:12 +0500

    Update lesson-6 homework.sql

--- lesson-6/homework/lesson-6 homework.sql
+++ lesson-6/homework/lesson-6 homework.sql
@@ -1,12 +1,9 @@
-create database lesson6_homework
+create database les6_homework
 
-
-
---------- 1ST PUZZLE
-
-SELECT *
-FROM INPUTTBL
-
+--1.Question: Explain at least two ways to find distinct values based on two columns.
+CREATE TABLE InputTbl ( col1 VARCHAR(10), col2 VARCHAR(10) ); INSERT INTO InputTbl (col1, col2) VALUES ('a', 'b'), ('a', 'b'), ('b', 'a'), ('c', 'd'), ('c', 'd'), ('m', 'n'), ('n', 'm');
+select *
+from InputTbl
 
 SELECT 
     CASE WHEN col1 < col2 THEN col1 ELSE col2 END AS col1,
@@ -16,39 +13,47 @@ GROUP BY
     CASE WHEN col1 < col2 THEN col1 ELSE col2 END,
     CASE WHEN col1 < col2 THEN col2 ELSE col1 END;
 
+--2.Removing Rows with All Zeroes.Question: If all the columns have zero values, then don’t show that row. In this case, we have to remove the 5th row while selecting data.
+CREATE TABLE TestMultipleZero ( A INT NULL, B INT NULL, C INT NULL, D INT NULL );
 
------------------- 2ND PUZZLE
+INSERT INTO TestMultipleZero(A,B,C,D) VALUES (0,0,0,1), (0,0,1,0), (0,1,0,0), (1,0,0,0), (0,0,0,0), (1,1,1,0);
 
 select *
-from testduplicatecount
+from TestMultipleZero
+where A !=0 OR B!=0 OR C!=0 OR D!=0
 
 
-select EmpName, count(empname) as Appearences
-from testduplicatecount
-group by EmpName
-having count(empname)>1
+--3.Find those with odd ids
+create table section1(id int, name varchar(20)) insert into section1 values (1, 'Been'), (2, 'Roma'), (3, 'Steven'), (4, 'Paulo'), (5, 'Genryh'), (6, 'Bruno'), (7, 'Fred'), (8, 'Andro')
 
-
-select EmpName, Appearences 
-from (
-	select EmpName, count(empname) as Appearences
-	from testduplicatecount
-	group by EmpName) as base_table
-where Appearences > 1
-
- 
-
---------------------- 3RD PUZZLE
 select *
-from groupbymultiplecolumns
+from section1
+where id%2 > 0
 
 
+--Puzzle 4: Person with the smallest id 
+select *
+from section1
+where id = (select min(id) from section1)
 
-select typ, 
-	count(case
-	when value1 = 'a' then 1
-	when value2 = 'a' then 1
-	when value3 = 'a' then 1
-	end) as number
-from groupbymultiplecolumns
-group by typ
+
+--Puzzle 5: Person with the highest id 
+select *
+from section1
+where id = (select max(id) from section1)
+
+
+--Puzzle 6: People whose name starts with b
+select *
+from section1
+where name like 'b%'
+
+
+--Puzle 7: Write a query to return only the rows where the code contains the literal underscore _ (not as a wildcard)
+CREATE TABLE ProductCodes ( Code VARCHAR(20) );
+
+INSERT INTO ProductCodes (Code) VALUES ('X-123'), ('X_456'), ('X#789'), ('X-001'), ('X%202'), ('X_ABC'), ('X#DEF'), ('X-999');
+
+select *
+from ProductCodes
+where Code like '%\_%' escape '\'
